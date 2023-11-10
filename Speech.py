4dc642c1b637a3e2f0d06670314358246201c053
RedSLeo <86766332+RedSLeo@users.noreply.github.com>
2023-11-10 01:02:21 -0500

    Update Speech.py

--- Speech.py
+++ Speech.py
@@ -1,12 +1,27 @@
-import random
-import time
-
 import speech_recognition as sr
 
 rec = sr.Recognizer()
 mic = sr.Microphone()
 
 with mic as source:
-    audio = rec.listen(source)
+    print("Adjusting for ambient noise. Please wait . . .")
+    rec.adjust_for_ambient_noise(source, duration = 5)
+    print("Ambient noise adjustment complete.")
 
-rec.recognize_google(audio)
+timeout_duration = 1
+
+with mic as source:
+    print("What is on your mind. . .")
+    audio = rec.listen(source)
+    try:
+        audio = rec.listen(source, timeout=timeout_duration)
+        print("Recording complete.")
+
+        # The recording audio is saved as a .wav file
+        with open("recorded_audio.wav", "wb") as f:
+            f.write(audio.get_wav_data())
+
+        result = rec.recognize_google(audio)
+        print("Recognized text:", result)
+    except sr.WaitTimeoutError:
+        print("I have not heard anything from you. Lemme know when you are ready.")
